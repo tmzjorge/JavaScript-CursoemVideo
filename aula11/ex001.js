c65e311836f27ec184ff9082228baa410b2f4b02
tmzjorge <116689948+tmzjorge@users.noreply.github.com>
2023-01-03 09:50:35 -0300

    desafio redes sociasi

---
+++ aula11/ex001.js
@@ -0,0 +1,11 @@
+// var vel = 80
+
+// vel>70 ? console.log(vel + 'Km/h. Acima do limite!') : console.log(vel + 'Km/h. Dentro do limite.')
+
+// var pais = 'Brasil'
+// pais=='Brasil' ? console.log('Você é brasileiro') : console.log('Você é estrangeiro')
+
+number = 2
+
+number%2 == 0? number *= 9 : number*= 8
+console.log(number)
